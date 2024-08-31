9433341c5da615f80e6928d7ed4db89826abdfe0
BDLBSESAY <BDLBSESAY>
2024-08-31 20:06:00 +0000

    first commit

---
+++ src/main/java/com/votingsystem/voting/ApplicationController.java
@@ -0,0 +1,4 @@
+package com.votingsystem.voting;
+
+public class ApplicationController {
+}
---
+++ src/main/java/com/votingsystem/voting/OnlineVotingSystemUsingFacialRecognitionApplication.java
@@ -0,0 +1,13 @@
+package com.votingsystem.voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineVotingSystemUsingFacialRecognitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineVotingSystemUsingFacialRecognitionApplication.class, args);
+    }
+
+}
